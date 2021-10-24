4ccd50f914389e1d89509424c49a3655b87bcef8
flore <flore@DESKTOP-DUBOHO8>
2021-10-24 22:26:41 +0300

    Add project files.

---
+++ DoughnutMachine.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Threading;
+
+namespace Florescu_Traian_Lab2
+{
+
+
+    class DoughnutMachine
+    {
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        public delegate void DoughnutCompleteDelegate();
+        public event DoughnutCompleteDelegate DoughnutComplete;
+        DispatcherTimer doughnutTimer;
+
+        private void InitializeComponent()
+        {
+            this.doughnutTimer = new DispatcherTimer();
+            this.doughnutTimer.Tick += new System.EventHandler(this.doughnutTimer_Tick);
+        }
+        public DoughnutMachine()
+        {
+            InitializeComponent();
+        }
+        private void doughnutTimer_Tick(object sender, EventArgs e)
+        {
+            Doughnut aDoughnut = new Doughnut(this.Flavor);
+            DoughnutComplete();
+        }
+        public bool Enabled
+        {
+            set
+            {
+                doughnutTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                doughnutTimer.Interval = new TimeSpan(0, 0, value);
+            }
+        }
+    }
+
+
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+
+
+
+    }
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mTimeOfCreation = DateTime.Now;
+            mFlavor = aFlavor;
+        }
+    }
+
+
+
+}
